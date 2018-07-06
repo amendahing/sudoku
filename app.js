728d1416319b8805a3fba211b01608d3b114903b
Amenda Hing <amendahing@gmail.com>
2018-07-06 11:18:12 -0700

    save board game

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ var game = [
 ];
 
 function sudokuGen(arr){
-    var output = "<table>";
+    var output = "<table>"
     for (var x = 0; x < arr.length; x++) {
-      output += genRow(arr[x])
+      // output += genRow(arr[x])
+      output += "<tr>"
+      for(var i = 0; i < arr[x].length; i++) {
+          if (arr[x][i] == 0) {
+              output+= `<td id='box-${x}-${i}'> <input type='text'> </td>`
+          }
+          else {
+              output+= `<td id='box-${x}-${i}'>${arr[x][i]}</td>`
+          }
+      }
+      output += "</tr>"
     }
     output += "</table>"
     return output;
@@ -33,6 +43,21 @@ var board = sudokuGen(game);
 document.getElementById('generateGame').innerHTML += board;
 
 
-var currentGame = [
+function currentGame(){
+    var boardArr = game
+    document.addEventListener('keydown', function(event){
+        // console.log(event)
+        var path = event.path[1].id
+        var value = event.key
+        var int = parseInt(value, 10)
+        var x = path.split('')[4]
+        var i = path.split('')[6]
+        // console.log(`${x}, ${i}`)
 
-]
+        boardArr[x][i] = int;
+        console.log(boardArr)
+    })
+    return boardArr
+}
+
+currentGame()
